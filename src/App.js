664d74ca285dc758c42a647b002c9e8677d6b741
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-10-05 09:23:10 +0200

    Navbar working.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import request from "./utils/request";
@@ -8,14 +8,10 @@ import Loading from "./components/Loading";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import LocationDetails from "./pages/LocationDetails";
 import { EstablishmentForm } from './EstablishmentForm';
-import { ThemeContext, themes } from './ThemeContext';
 
 function App() {
   let [locations, setLocations] = useState([]);
   let [establishments, setEstablishments] = useState([]);
-
-  /* Use Theme Context (with a hook) */
-  let themeContext = useContext(ThemeContext);
 
   let {
     loading,
@@ -116,39 +112,6 @@ function App() {
                   render={() => (
                       <>
                         <a
-    <div className="App">
-      <header className="App-header">
-        {isAuthenticated && (
-          <a
-            className="App-link Logout-link"
-            href="#"
-            onClick={handleLogoutClick}
-          >
-            Logout
-          </a>
-        )}
-        <h1 style={{ color: themes[themeContext.theme].foreground }}>Welcome on OpenSunday</h1>
-        <h2>Find how to pass this dull day !</h2>
-        <br/>
-        <BrowserRouter>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={() => (
-                <>
-                  <a
-                    className="App-link"
-                    href="#"
-                    onClick={handleLocationsClick}
-                  >
-                    Get Locations
-                  </a>
-                  {locations && locations.length > 0 && (
-                    <ul className="Locations-List">
-                      {locations.map((location) => (
-                        <li key={location.id}>
-                          <Link
                             className="App-link"
                             href="#"
                             onClick={handleLocationsClick}
