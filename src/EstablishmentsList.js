1a1694dfd6df095e835bc07bbc556648e65c3ab9
Nelson Micheloud <nelson.michelou@hes-so.ch>
2020-10-16 15:30:55 +0200

    get est. by type 2 (not finished)

--- src/EstablishmentsList.js
+++ src/EstablishmentsList.js
@@ -1,15 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext, useEffect} from "react";
 import "./App.css";
 import { ThemeContext, themes } from './ThemeContext';
 import {Link} from "react-router-dom";
 import request from "./utils/request";
 import endpoints from "./endpoints.json";
 import {useAuth0} from "@auth0/auth0-react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 export function EstablishmentsList(){
     let themeContext = useContext(ThemeContext);
     let [establishments, setEstablishments] = useState([]);
-
+    let [establishmentsTypes, setEstablishmentsTypes] = useState([]);
+    let [establishmentTypeSelected, setEstablishmentTypeSelected] = useState([]);
     let {
         loading,
         loginWithRedirect,
@@ -18,53 +23,84 @@ export function EstablishmentsList(){
         isAuthenticated,
     } = useAuth0();
 
+    const types = ["All", "2", "3"];
+
+    const handleSelect=(e)=>{
+        console.log(e);
+        setEstablishmentTypeSelected(e)
+
+    }
+
     //Handle Establishments
     let handleEstablishmentsClick = async (e) => {
         e.preventDefault();
-        let establishments = await request(
-            `${process.env.REACT_APP_SERVER_URL}${endpoints.establishments}`,
-            getAccessTokenSilently,
-            loginWithRedirect
-        );
+        console.log(establishmentTypeSelected);
 
-        if (establishments && establishments.length > 0) {
-            console.log(establishments);
-            setEstablishments(establishments);
+        if(establishmentTypeSelected === "All") {
+            let establishments = await request(
+                `${process.env.REACT_APP_SERVER_URL}${endpoints.establishments}`,
+                getAccessTokenSilently,
+                loginWithRedirect
+            );
+
+            console.log("all");
+
+            if (establishments && establishments.length > 0) {
+                console.log(establishments);
+                setEstablishments(establishments);
+            }
+        }else{
+            let establishments = await request(
+                `${process.env.REACT_APP_SERVER_URL}${endpoints.establishmentsByType}${establishmentTypeSelected}`,
+                getAccessTokenSilently,
+                loginWithRedirect
+            );
+
+            console.log("2 or 3");
+
+            if (establishments && establishments.length > 0) {
+                console.log(establishments);
+                setEstablishments(establishments);
+            }
         }
+
+
     };
 
-    //Handle Establishments
-    let handleEstablishmentsTypesClick = async (e) => {
-        e.preventDefault();
-        let establishments = await request(
-            `${process.env.REACT_APP_SERVER_URL}${endpoints.establishments}`,
-            getAccessTokenSilently,
-            loginWithRedirect
-        );
 
-        if (establishments && establishments.length > 0) {
-            console.log(establishments);
-            setEstablishments(establishments);
-        }
-    };
 
 
     return(
         <>
             <h2 style={{color: themes[themeContext.theme].foreground}}>List of Establishments</h2>
+
+            {/*<label>
+                Establishment type :
+                <select value={establishmentTypeSelected} onChange={setEstablishmentTypeSelected}>
+                    <option value="All">All</option>
+                    <option value="2">Restaurant</option>
+                    <option value="3">Cinema</option>
+                </select>
+            </label>*/}
+
+            <DropdownButton
+                alignRight
+                title="Choose type of establishment "
+                id="dropdown-menu-align-right"
+                onSelect={handleSelect}
+            >
+                <Dropdown.Item eventKey="All">All</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Restaurant</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Cinema</Dropdown.Item>
+            </DropdownButton>
+
             <button
                 onClick={handleEstablishmentsClick}
             >
                 Get Establishments
             </button>
-            <label>
-                Establishment type :
-                <select>
-                    <option value="All">All</option>
-                    <option value="Restaurant">Restaurant</option>
-                    <option value="Cinema">Cinema</option>
-                </select>
-            </label>
+
+            <h3>List of all {establishmentTypeSelected}</h3>
             <ul className="EstablishmentsList">
                 {establishments.map((establishment) => (
                     <li key={establishment.id}>
--- src/components/Establishment.js
+++ src/components/Establishment.js
@@ -7,7 +7,10 @@ export default function Establishment(props) {
     return (
         <div className="establishment">
             <h2>{name}</h2>
+            <div>id : {id}</div>
             <div>{address}</div>
+            <div>{latitude}</div>
+            <div>{longitude}</div>
             <a href={url}>{url}</a>
         </div>
     );
