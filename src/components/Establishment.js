eebf17d271e49683d2f1b7d88a8b814fcf18d5ff
Nelson Micheloud <nelson.michelou@hes-so.ch>
2020-10-26 11:21:42 +0100

    Share buttons added on est.

--- src/components/Establishment.js
+++ src/components/Establishment.js
@@ -3,11 +3,14 @@ import request from "../utils/request";
 import endpoints from "../endpoints.json";
 import {useAuth0} from "@auth0/auth0-react";
 import {Map, Marker, TileLayer} from "react-leaflet";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {EmailShareButton, EmailIcon, TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon} from "react-share";
+
 
 
 export default function Establishment(props) {
-
     const { id, name, latitude, longitude, address, url, establishmentTypeId, locationId} = props;
+    const pageUrl = window.location.href
     let {
         loading,
         loginWithRedirect,
@@ -37,8 +40,35 @@ export default function Establishment(props) {
 
     return (
         <div className="establishment">
+
             <div>Id : {id}</div>
             <h2>Name : {name}</h2>
+            <CopyToClipboard text={pageUrl}>
+                <button>Copy URL to the clipboard</button>
+            </CopyToClipboard>
+            <div>
+                <EmailShareButton
+                    subject={name}
+                    body={"Hi,\nI just discovered this amazing establishment : " + name + ", on the app OpenSunday \nIt's open on Sunday !\n"}
+                    url={pageUrl}
+                    className={"shareButton"}>
+                    <EmailIcon size={32} round />
+                </EmailShareButton>
+                <FacebookShareButton
+                    url={pageUrl}
+                    quote={name}
+                    className="Demo__some-network__share-button"
+                >
+                    <FacebookIcon size={32} round />
+                </FacebookShareButton>
+                <TwitterShareButton
+                    url={pageUrl}
+                    title={name}
+                    className="shareButton"
+                >
+                    <TwitterIcon size={32} round />
+                </TwitterShareButton>
+            </div>
             <div>Latitude : {latitude}</div>
             <div>Longitude : {longitude}</div>
             <div>Address : {address}</div>
